bb43c2210145291743bfb204a27a5fd537244767
Rick Hambrook <rick@hambrook.co.nz>
2012-09-25 00:55:59 +1200

    Fixed: #links where being opened in new windows. Oops.

--- js/functions.js
+++ js/functions.js
@@ -3,14 +3,14 @@ jQuery(document).ready(function() {
 		return false;
 	}
 	if (extlinkopts.newwindow) {
-		var sel = extlinkopts.newwparent+" a[href]:not([href*='"+location.hostname+"'])";
+		var sel = extlinkopts.newwparent+" a[href]:not([href*='"+location.hostname+"'],[href^=#])";
 		if (extlinkopts.newwexclude) { sel = sel+":not("+extlinkopts.newwexclude+")" };
 		$("body").delegate(sel, "click", function() {
 			window.open(this.href); return false;
 		});
 	}
 	if (extlinkopts.rel||extlinkopts.addclass) {
-		$(extlinkopts.rorcparent+" a[href]:not([href*='"+location.hostname+"'],:has(img))").each(function(i,v) {
+		$(extlinkopts.rorcparent+" a[href]:not([href*='"+location.hostname+"'],[href^=#],:has(img))").each(function(i,v) {
 			/* Exclude empty tags (eg if used as a clickable area with background image) */
 			if ($(v).text().replace('/(&nbsp;|\s)/', '')) {
 				if (extlinkopts.rel) {
