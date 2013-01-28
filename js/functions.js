0410c5719104d115b9c872cd6282e384135a621f
Rick Hambrook <rick@hambrook.co.nz>
2013-01-28 16:40:26 +1300

    Fixed: href*=// selector was causing issues. Fixed: Proper escaping on \s, spacing, tabs, indenting etc. Note: Still uses jQuery delegate()... Want to use on() but can't risk breaking sites that use older jQuery versions.

--- js/functions.js
+++ js/functions.js
@@ -3,16 +3,18 @@ jQuery(document).ready(function() {
 		return false;
 	}
 	if (extlinkopts.newwindow) {
-		var sel = extlinkopts.newwparent+" a[href*=//]:not([href*='"+location.hostname+"'],[href^=#])";
-		if (extlinkopts.newwexclude) { sel = sel+":not("+extlinkopts.newwexclude+")" };
+		var sel = extlinkopts.newwparent+" a[href*='//']:not([href*='"+location.hostname+"'],[href^=#])";
+		if (extlinkopts.newwexclude) { sel = sel+":not("+extlinkopts.newwexclude+")" }
 		$("body").delegate(sel, "click", function() {
-			window.open(this.href); return false;
+			window.open(this.href);
+			return false;
 		});
 	}
 	if (extlinkopts.rel||extlinkopts.addclass) {
-		$(extlinkopts.rorcparent+" a[href*=//]:not([href*='"+location.hostname+"'],[href^=#],:has(img))").each(function(i,v) {
+		$(extlinkopts.rorcparent+" a[href*='//']:not([href*='"+location.hostname+"'],[href^=#],:has(img))")
+        .each(function(i,v) {
 			/* Exclude empty tags (eg if used as a clickable area with background image) */
-			if ($(v).text().replace('/(&nbsp;|\s)/', '')) {
+			if ($(v).text().replace('/(&nbsp;|\\s)/', '')) {
 				if (extlinkopts.rel) {
 					$(this).attr('rel', 'external '+$(this).attr('rel'));
 				}
@@ -22,5 +24,4 @@ jQuery(document).ready(function() {
 			}
 		})
 	}
-
 });
